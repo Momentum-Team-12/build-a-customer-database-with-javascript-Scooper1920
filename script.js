3a46eaed736c85a4a0bd4cd2d686fe1dddc75aee
Shawna Cooper <scooper1920@gmail.com>
2022-04-05 15:15:58 -0400

    (more homework)

--- script.js
+++ script.js
@@ -2,9 +2,6 @@ console.log("What it do?");
 
 console.log(customers[0]);
 
-
-
-
 const root = document.getElementById("root");
 console.log("deeply rooted");
 
@@ -13,17 +10,20 @@ for (let customer of customers) {
   const customerCard = document.createElement("div");
   customerCard.classList.add("customer-card");
 
-
-
   root.appendChild(customerCard);
   customerCard.innerHTML = `
   <img src="${customer.picture.thumbnail}"/>
-<p class="name" >${customer.name.title}. ${customer.name.first} ${customer.name.last}</p>
+  
+  <h2 class=name> ${customer.name.first} ${customer.name.last}</h2>
+  <div>${customer.location.street.number} ${customer.location.street.name}</div>
+  <div>${customer.location.city}, ${nameToAbbr(customer.location.state)} ${customer.location.postcode}</div>
+  
+
+
+
 
 
 `;
-function capitalizeFirstLetter() {
-    return customer.name.title.charAt(0).toUpperCase() + customerCard.slice(1);
-}
-
+  // function capitalizeFirstLetter() {
+  //     return customer.name.title.charAt(0).toUpperCase() + customerCard.slice(1);
 }
